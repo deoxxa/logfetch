b418b1bb805e25ac534b8743b67eb149e3135924
deoxxa <deoxxa@fknsrs.biz>
2015-07-21 19:31:36 +1000

    add logfmt parser

---
+++ logfmt.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/kr/logfmt"
+)
+
+type logfmtHandler map[string]string
+
+func (l *logfmtHandler) HandleLogfmt(key, val []byte) error {
+	(*l)[string(key)] = string(val)
+	return nil
+}
+
+func parseLogfmt(in <-chan map[string]interface{}, key string) <-chan map[string]interface{} {
+	return mapOver(in, func(m map[string]interface{}) map[string]interface{} {
+		f, ok := m[key]
+		if !ok {
+			return m
+		}
+
+		s, ok := f.(string)
+		if !ok {
+			return m
+		}
+
+		e := make(logfmtHandler)
+		if err := logfmt.Unmarshal([]byte(s), &e); err != nil {
+			return m
+		}
+
+		for k, v := range e {
+			m[k] = v
+		}
+
+		return m
+	})
+}
